fec0138eef51c55c96625905e31fa593040cc9b9
Miguel Lopes <miguelgints@gmail.com>
2024-02-28 20:54:02 -0400

    adição de comentários

--- json_utils.py
+++ json_utils.py
@@ -1,21 +1,26 @@
-#Ler o JSON caso não exista o arquivo ele cria o arquivo json no diretório do arquivo
 import json
 
+# Função para ler o arquivo JSON
 def read_json(path_):
     data = []
     try:
+        # Tenta abrir o arquivo JSON e carregar os dados
         with open(path_, 'r', encoding='utf-8') as file:
             data = json.load(file)
     except FileNotFoundError:
+        # Se o arquivo não for encontrado, cria um novo arquivo JSON vazio
         save_employee([], path_)
     return data
 
-#Salva os dados no arquivo JSON
+# Função para salvar os dados no arquivo JSON
 def save_employee(data_, path_):
     try:
+        # Tenta abrir o arquivo JSON para escrita e salvar os dados
         with open(path_, 'w', encoding='utf-8') as file:
+            # Salva os dados no arquivo com formatação e codificação adequadas
             json.dump(data_, file, indent=2, ensure_ascii=False)
         return True
     except Exception as e:
+        # Em caso de erro, imprime a mensagem de erro
         print(f'Error ao salvar os dados {e}')
         return False
